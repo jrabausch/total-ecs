5194e6698b0ee4686d1bab156a1c57b2cac958fd
Johannes Rabausch <mail@jrabausch.de>
2023-08-10 17:04:23 +0000

    Added IndexPool tests

--- spec/IndexPoolSpec.ts
+++ spec/IndexPoolSpec.ts
@@ -1,6 +1,6 @@
 import { IndexPool } from '../src';
 
-describe('Manager', () => {
+describe('IndexPool', () => {
 
   let indexPool: IndexPool;
 
@@ -10,5 +10,49 @@ describe('Manager', () => {
 
   it('should be self', () => {
     expect(indexPool).toBeInstanceOf(IndexPool);
+  });
+
+  it('should produce indexes', () => {
+    expect(indexPool.get()).toBe(0);
+    expect(indexPool.get()).toBe(1);
+    expect(indexPool.get()).toBe(2);
+    expect(indexPool.get()).toBe(3);
+    expect(indexPool.get()).toBe(4);
+    expect(indexPool.get()).toBe(5);
+  });
+
+  it('should free indexes', () => {
+    expect(indexPool.get()).toBe(0);
+    expect(indexPool.get()).toBe(1);
+    expect(indexPool.get()).toBe(2);
+
+    indexPool.free(2);
+    expect(indexPool.get()).toBe(2);
+
+    indexPool.free(0);
+    expect(indexPool.get()).toBe(0);
+
+    expect(indexPool.get()).toBe(3);
+
+    indexPool.free(1);
+    expect(indexPool.get()).toBe(1);
+  });
+
+  it('should provide size', () => {
+    expect(indexPool.size()).toBe(0);
+
+    const i0 = indexPool.get();
+    const i1 = indexPool.get();
+
+    expect(indexPool.size()).toBe(2);
+
+    indexPool.free(i0);
+    indexPool.free(i1);
+
+    expect(indexPool.size()).toBe(2);
+
+    expect(indexPool.get()).toBe(i1);
+
+    expect(indexPool.size()).toBe(2);
   });
 });
